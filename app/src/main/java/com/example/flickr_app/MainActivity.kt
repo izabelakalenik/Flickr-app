fdaea810f854f5853da9ead1752063e7d82e9c03
izabela <iza.kalenik@gmail.com>
2024-01-10 15:27:43 +0100

    add dirs, colors, theme

--- app/src/main/java/com/example/flickr_app/MainActivity.kt
+++ app/src/main/java/com/example/flickr_app/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.flickr_app.ui.theme.Flickr_appTheme
+import com.example.flickr_app.ui.theme.FlickrAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Flickr_appTheme {
+            FlickrAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Flickr_appTheme {
+    FlickrAppTheme {
         Greeting("Android")
     }
 }
